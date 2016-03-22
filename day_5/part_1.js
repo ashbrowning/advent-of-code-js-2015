a93f3edbe11761f463bb77de13bf8063b3cf0eb1
Ash Browning <ambrowning89@gmail.com>
2016-03-22 21:59:07 +0000

    feat(day5): adding solution to day 5

---
+++ day_5/part_1.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt', { encoding: 'utf8' });
+const words = input.split('\n');
+const vowelsSet = new Set(['a', 'e', 'i', 'o', 'u']);
+const bannedSet = new Set(['ab', 'cd', 'pq', 'xy']);
+
+let niceCount = 0;
+
+function checkVowel(letter, vowelCount) {
+  if (vowelsSet.has(letter)) {
+    vowelCount.push(letter);
+  }
+  return vowelCount;
+}
+
+for (let i = 0; i < words.length; i++) {
+  const word = words[i];
+  const vowelCount = [];
+
+  checkVowel(word[0], vowelCount);
+  let hasDouble = false;
+  let hasThreeVowels = false;
+  let isNaughty = false;
+
+  for (let j = 1; j < word.length; j++) {
+    if (bannedSet.has(word[j - 1] + word[j])) {
+      isNaughty = true;
+      break;
+    }
+
+    if (!hasThreeVowels) {
+      checkVowel(word[j], vowelCount);
+      if (vowelCount.length >= 3) {
+        hasThreeVowels = true;
+      }
+    }
+
+    if (!hasDouble) {
+      if (word[j] === word[j - 1]) {
+        hasDouble = true;
+      }
+    }
+  }
+
+  if (!isNaughty && hasDouble && hasThreeVowels) {
+    niceCount++;
+  }
+}
+
+console.log(niceCount);
