4aa19ab628acbe7373905eaada73761eb2988d6d
Ash Browning <ambrowning89@gmail.com>
2016-03-28 22:07:11 +0100

    feat(testing): adding tests
    The number of refactors and rechecking solutions I'm doing, the test suite
    should make life a bit easier..

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 'use strict';
 
-const fs = require('fs');
-const moment = require('moment');
-
+const launcher = require('./launcher.js');
 const dayArg = process.argv[2];
 const partArg = process.argv[3];
 
-const solution = require(`./day_${dayArg}/part_${partArg}.js`);
-const input = fs.readFileSync(
-    `${__dirname}/day_${dayArg}/input.txt`,
-    { encoding: 'utf8' }
-  ).split('\n');
-
-const startMoment = moment();
-const result = solution(input);
-const duration = moment().diff(startMoment);
-
-console.log('Answer:', result);
-console.log('Runtime:', moment(duration).format('m:s.SSS'));
+launcher(dayArg, partArg);
---
+++ test.spec.js
@@ -0,0 +1,61 @@
+'use strict';
+/* global describe it expect */
+
+const launcher = require('./launcher.js');
+
+describe('Advent of code tests', () => {
+  it('should give correct answers to day 1 part 1', () => {
+    expect(launcher(1, 1)).toBe(138);
+  });
+  it('should give correct answers to day 1 part 2', () => {
+    expect(launcher(1, 2)).toBe(1771);
+  });
+  it('should give correct answers to day 2 part 1', () => {
+    expect(launcher(2, 1)).toBe(1598415);
+  });
+  it('should give correct answers to day 2 part 2', () => {
+    expect(launcher(2, 2)).toBe(3812909);
+  });
+  it('should give correct answers to day 3 part 1', () => {
+    expect(launcher(3, 1)).toBe(2592);
+  });
+  it('should give correct answers to day 3 part 2', () => {
+    expect(launcher(3, 2)).toBe(2360);
+  });
+  it('should give correct answers to day 4 part 1', () => {
+    expect(launcher(4, 1)).toBe(346386);
+  });
+  it('should give correct answers to day 4 part 2', () => {
+    expect(launcher(4, 2)).toBe(9958218);
+  });
+  it('should give correct answers to day 5 part 1', () => {
+    expect(launcher(5, 1)).toBe(238);
+  });
+  it('should give correct answers to day 5 part 2', () => {
+    expect(launcher(5, 2)).toBe(69);
+  });
+  it('should give correct answers to day 6 part 1', () => {
+    expect(launcher(6, 1)).toBe(543903);
+  });
+  it('should give correct answers to day 6 part 2', () => {
+    expect(launcher(6, 2)).toBe(14687245);
+  });
+  it('should give correct answers to day 7 part 1', () => {
+    expect(launcher(7, 1)).toBe(956);
+  });
+  it('should give correct answers to day 7 part 2', () => {
+    expect(launcher(7, 2)).toBe(40149);
+  });
+  it('should give correct answers to day 8 part 1', () => {
+    expect(launcher(8, 1)).toBe(1350);
+  });
+  it('should give correct answers to day 8 part 2', () => {
+    expect(launcher(8, 2)).toBe(2085);
+  });
+  it('should give correct answers to day 9 part 1', () => {
+    expect(launcher(9, 1)).toBe(117);
+  });
+  it('should give correct answers to day 9 part 2', () => {
+    expect(launcher(9, 2)).toBe(909);
+  });
+});
